1edd21e604f7b7341bb1d774dc20d239a6882cb5
MonMar <MonMar@192.168.254.4>
2018-09-16 20:46:45 +0800

    Screen render() ArrayOutOfBound

--- Rain/com/thecherno/rain/graphics/Screen.java
+++ Rain/com/thecherno/rain/graphics/Screen.java
@@ -5,7 +5,7 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 	
-	int time = 0;
+	int xtime = 0, ytime = 50;
 	int counter = 0;
 	
 	public Screen(int width, int height) {
@@ -24,12 +24,14 @@ public class Screen {
 	// draws the pixels for the game in nested for loop 
 	public void render() {
 		counter++;
-		if(counter % 100 == 0) {
-			time++;
-		}
+		if(counter % 100 == 0) xtime++;
+		if(counter % 800 == 0) ytime++;
+		
 		for(int y = 0; y < height; y++) {
+			if(ytime >= height) break;
 			for(int x = 0; x < width; x++) {
-				pixels[time + time * width] = 0x00FFFF;
+				if(xtime >= width) break;
+				pixels[xtime + ytime * width] = 0x00FFFF;
 			}
 		}
 	}
