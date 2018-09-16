9eaf86bc28730bb16a05f4e77359fca181ea2ac2
MonMar <MonMar@192.168.254.4>
2018-09-16 19:35:11 +0800

    Created the Screen class, created a graphics package.

--- Rain/com/thecherno/rain/Game.java
+++ Rain/com/thecherno/rain/Game.java
@@ -10,12 +10,16 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.thecherno.rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable{
 	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	
+	private Screen screen;
 	
 	private Thread thread;
 	private boolean running = false;
@@ -27,6 +31,8 @@ public class Game extends Canvas implements Runnable{
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
+		screen = new Screen(width, height);
+		
 		frame = new JFrame();
 	}
 	
