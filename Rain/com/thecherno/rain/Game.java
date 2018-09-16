efd22825bb3c9e9c11ab0d6e644d9238eda7543b
MonMar <MonMar@192.168.254.4>
2018-09-16 19:22:37 +0800

    added BufferedImage Object and pixels variable in Game class

--- Rain/com/thecherno/rain/Game.java
+++ Rain/com/thecherno/rain/Game.java
@@ -4,14 +4,13 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
+import java.awt.image.*;
 
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
-	
-	
 	private static final long serialVersionUID = 1L;
+	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
@@ -19,6 +18,9 @@ public class Game extends Canvas implements Runnable{
 	private Thread thread;
 	private boolean running = false;
 	private JFrame frame;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
