8f4f0deaecba39ec957e9aed4ea9569d3bb3e0fc
MonMar <MonMar@192.168.254.4>
2018-09-16 21:53:08 +0800

    Timer in run() method.

--- Rain/com/thecherno/rain/Game.java
+++ Rain/com/thecherno/rain/Game.java
@@ -16,7 +16,7 @@ public class Game extends Canvas implements Runnable{
 	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
-	public static int height = width / 16 * 9;
+	public static int height = 168;
 	public static int scale = 3;
 	
 	private Screen screen;
@@ -53,11 +53,20 @@ public class Game extends Canvas implements Runnable{
 	
 	@Override
 	public void run() {
+		long lastTime = System.nanoTime();
+		final double ns = 1_000_000_000.0 / 60.0;
+		double delta = 0;
 		while(running) {
-			System.out.println("Running...");
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				update();
+				delta--;
+			}
 			render();
 		}
+		stop();
 	}
 	
 	public void update() {
