6145255f97515eaa84984a69af5b7a8d9e199243
MonMar <MonMar@192.168.254.6>
2018-12-27 22:53:32 +0800

    Added sprite

--- Rain/com/thecherno/rain/graphics/Screen.java
+++ Rain/com/thecherno/rain/graphics/Screen.java
@@ -38,8 +38,7 @@ public class Screen {
 			int yy = y + yOffSet;
 			for(int x = 0; x < width; x++) {
 				int xx = x + xOffSet;
-				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE;
-				pixels[x + y * width] = tiles[tileIndex];
+				pixels[x + y * width] = Sprite.grass.pixels[(x & 15) + (y & 15) * Sprite.grass.SIZE];
 			}
 		}
 	}
--- Rain/com/thecherno/rain/graphics/SpriteSheet.java
+++ Rain/com/thecherno/rain/graphics/SpriteSheet.java
@@ -1,6 +1,7 @@
 package com.thecherno.rain.graphics;
 
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -10,6 +11,8 @@ public class SpriteSheet {
 	private String path;
 	public final int SIZE;
 	public int[] pixels;
+	
+	public static SpriteSheet tiles = new SpriteSheet("/textures/spritesheet.png", 256); 
 	
 	public SpriteSheet(String path, int size) {
 		this.path = path;
